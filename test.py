269db98ee332951dac692cb6a551b0c3e01c8b0a
Arvid <carlarvidsundqvist@gmail.com>
2019-01-22 16:21:36 +0100

    Things

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ cv2.namedWindow("test")
 
 img_counter = 0
 
-#face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 while True:
     ret, frame = cam.read()
 
@@ -26,11 +26,14 @@ while True:
         print("{} written!".format(img_name))
         img_counter += 1
 
+    cv2.imshow("Raw frame", frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        face_frame = frame[y:y + h, x:x + w]
-    cv2.imshow("Face test", face_frame)
+
+    for (x, y, w, h) in faces:
+        face_frame = frame[y - 20:y + h + 20, x - 20:x + w + 20]
+        cv2.imshow("Face frame", face_frame)
+
 
 cam.release()
 
